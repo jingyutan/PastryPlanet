2c49f24e5a3a7cd1d34c7d52f97197f04d895d21
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-21 14:25:55 +0800

    Added SeedData.cs into Models folder

---
+++ PastryPlanet/Models/SeedData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PastryPlanet.Data;
+
+namespace PastryPlanet.Models
+{
+    public class SeedData
+    {
+        public static void Initialize (IServiceProvider serviceProvider)
+        {
+            using (var context = new PastryPlanetContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<PastryPlanetContext>>()))
+            {
+                // Look for any Products
+                if (context.Product.Any())
+                {
+                    return; // DB has been seeded
+                }
+                
+            }
+        }
+    }
+}
