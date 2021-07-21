32b0d5d1845caa4d60e3ef2e8e443a87ea4d14a9
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-21 14:15:31 +0800

    Added folder "Products" under Pages Added scaffold item into Pages/Products for Products class

---
+++ PastryPlanet/Data/PastryPlanetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PastryPlanet.Models;
+
+namespace PastryPlanet.Data
+{
+    public class PastryPlanetContext : DbContext
+    {
+        public PastryPlanetContext (DbContextOptions<PastryPlanetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PastryPlanet.Models.Product> Product { get; set; }
+    }
+}
---
+++ PastryPlanet/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PastryPlanet.Data;
+using PastryPlanet.Models;
+
+namespace PastryPlanet.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly PastryPlanet.Data.PastryPlanetContext _context;
+
+        public DetailsModel(PastryPlanet.Data.PastryPlanetContext context)
+        {
+            _context = context;
+        }
+
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _context.Product.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
