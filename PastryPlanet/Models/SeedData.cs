942e207e41e12ce3998ba430f79ee7cc8c77159b
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-21 14:34:49 +0800

    Added 3 products into SeedData.cs

--- PastryPlanet/Models/SeedData.cs
+++ PastryPlanet/Models/SeedData.cs
@@ -21,7 +21,37 @@ namespace PastryPlanet.Models
                 {
                     return; // DB has been seeded
                 }
-                
+                context.Product.AddRange(
+                    new Product
+                    {
+                        ID = 1,
+                        Name = "Chewey Chocolate Chip Cookies",
+                        Description = "Freshly baked, chewey cookies with pockets of flowey dark chocolate",
+                        Price = 2.90,
+                        QuantityInStock = 20
+                    },
+
+                    new Product
+                    {
+                        ID = 2,
+                        Name = "Flakey Crossaints",
+                        Description = "Perfectly laminated, buttery and decadent pastry.",
+                        Price = 5.90,
+                        QuantityInStock = 15
+                    },
+
+                    new Product
+                    {
+                        ID = 3,
+                        Name = "Classic NY Cheesecake",
+                        Description = "Indulgent and creamy with a crisp Graham Cracker crust.",
+                        Price = 4.90,
+                        QuantityInStock = 25
+                    }
+                );
+                context.SaveChanges();
+                    
+                 
             }
         }
     }
