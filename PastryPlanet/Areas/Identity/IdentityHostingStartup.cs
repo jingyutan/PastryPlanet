6111d34277e50857477ac1454c5fde7951737dc9
x89302374 <s10204879@connect.np.edu.sg>
2021-07-21 21:57:29 +0800

    Add login/register/authentication
    (according to week4 practical - Razor Pages Security 1 – Registration and Authentication)

---
+++ PastryPlanet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PastryPlanet.Data;
+using PastryPlanet.Models;
+
+[assembly: HostingStartup(typeof(PastryPlanet.Areas.Identity.IdentityHostingStartup))]
+namespace PastryPlanet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
