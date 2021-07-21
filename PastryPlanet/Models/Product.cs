217972fb51fa8516e12b839ad553ef6e954fd273
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-21 14:12:58 +0800

    Added models folder, added Product.cs inside models folder

---
+++ PastryPlanet/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PastryPlanet.Models
+{
+    public class Product
+    {
+// ------------------------------ Section for constructors ------------------------------
+        public int ID { get; set; } // Primary key for products
+        public string Name { get; set; } // Name of the product
+        public string Description { get; set; } // Description of product
+        public double Price { get; set; } // Price of the product
+        public int QuantityInStock { get; set; } // Stock of the product
+
+        /*public string Category { get; set; }  Category of the product 
+                                              (might not be needed, 
+                                                comment out first)*/
+
+    }
+}
