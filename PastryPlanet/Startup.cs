8a88434896961d31a9a341e74506986670d85e54
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-27 20:59:31 +0800

    didstuff

--- PastryPlanet/Startup.cs
+++ PastryPlanet/Startup.cs
@@ -28,7 +28,8 @@ namespace PastryPlanet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().
+                AddRazorRuntimeCompilation();
 
             services.AddDbContext<PastryPlanetContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("PastryPlanetContext")));
