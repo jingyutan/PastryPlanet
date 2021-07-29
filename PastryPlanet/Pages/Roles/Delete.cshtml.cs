b14296a51320d64e4535466f35e0e440abcb4a4a
Muhammad Aidel Sufyan Abdul Manap <aidelsufyan21@gmail.com>
2021-07-30 01:53:37 +0800

    Added codes for Pages\Roles\Delete.cshtml and Pages\Roles\Delete.cshtml.cs

--- PastryPlanet/Pages/Roles/Delete.cshtml.cs
+++ PastryPlanet/Pages/Roles/Delete.cshtml.cs
@@ -1,59 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using PastryPlanet.Data;
 using PastryPlanet.Models;
-
 namespace PastryPlanet.Pages.Roles
 {
-    public class DeleteModel : PageModel
-    {
-        private readonly PastryPlanet.Data.PastryPlanetContext _context;
-
-        public DeleteModel(PastryPlanet.Data.PastryPlanetContext context)
-        {
-            _context = context;
-        }
-
-        [BindProperty]
-        public ApplicationRole ApplicationRole { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            ApplicationRole = await _context.Roles.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (ApplicationRole == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            ApplicationRole = await _context.Roles.FindAsync(id);
-
-            if (ApplicationRole != null)
-            {
-                _context.Roles.Remove(ApplicationRole);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
-        }
-    }
+	public class DeleteModel : PageModel
+	{
+		private readonly RoleManager<ApplicationRole> _roleManager;
+		public DeleteModel(RoleManager<ApplicationRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+		[BindProperty]
+		public ApplicationRole ApplicationRole { get; set; }
+		public async Task<IActionResult> OnGetAsync(string id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			ApplicationRole = await _roleManager.FindByIdAsync(id);
+			if (ApplicationRole == null)
+			{
+				return NotFound();
+			}
+			return Page();
+		}
+		public async Task<IActionResult> OnPostAsync(string id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			ApplicationRole = await _roleManager.FindByIdAsync(id);
+			IdentityResult roleRuslt = await _roleManager.DeleteAsync(ApplicationRole);
+			return RedirectToPage("./Index");
+		}
+	}
 }
