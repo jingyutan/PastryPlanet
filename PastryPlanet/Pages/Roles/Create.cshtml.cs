2939377cb20a8b28d69aa83adc0ff1478af65b27
Muhammad Aidel Sufyan Abdul Manap <aidelsufyan21@gmail.com>
2021-07-30 01:52:10 +0800

    Added codes for Pages\Roles\Create.cshtml and Pages\Roles\Roles\Create.cshtml.cs

--- PastryPlanet/Pages/Roles/Create.cshtml.cs
+++ PastryPlanet/Pages/Roles/Create.cshtml.cs
@@ -1,45 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using PastryPlanet.Data;
 using PastryPlanet.Models;
-
+using System;
+using System.Threading.Tasks;
 namespace PastryPlanet.Pages.Roles
 {
-    public class CreateModel : PageModel
-    {
-        private readonly PastryPlanet.Data.PastryPlanetContext _context;
-
-        public CreateModel(PastryPlanet.Data.PastryPlanetContext context)
-        {
-            _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
-
-        [BindProperty]
-        public ApplicationRole ApplicationRole { get; set; }
-
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Roles.Add(ApplicationRole);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
-    }
+	public class CreateModel : PageModel
+	{
+		private readonly RoleManager<ApplicationRole> _roleManager;
+		public CreateModel(RoleManager<ApplicationRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+		[BindProperty]
+		public ApplicationRole ApplicationRole { get; set; }
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			ApplicationRole.CreatedDate = DateTime.UtcNow;
+			ApplicationRole.IPAddress =
+		   Request.HttpContext.Connection.RemoteIpAddress.ToString();
+			IdentityResult roleRuslt = await _roleManager.CreateAsync(ApplicationRole);
+			return RedirectToPage("Index");
+		}
+	}
 }
