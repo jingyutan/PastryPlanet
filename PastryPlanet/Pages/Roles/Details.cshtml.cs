ed46a3321f0c0813466868ae78849d264b418f78
Muhammad Aidel Sufyan Abdul Manap <aidelsufyan21@gmail.com>
2021-07-30 01:55:34 +0800

    Added codes for Pages\Roles\Details.cshtml and Pages\Roles\Details.cshtml.cs

--- PastryPlanet/Pages/Roles/Details.cshtml.cs
+++ PastryPlanet/Pages/Roles/Details.cshtml.cs
@@ -2,39 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using PastryPlanet.Data;
 using PastryPlanet.Models;
-
 namespace PastryPlanet.Pages.Roles
 {
-    public class DetailsModel : PageModel
-    {
-        private readonly PastryPlanet.Data.PastryPlanetContext _context;
-
-        public DetailsModel(PastryPlanet.Data.PastryPlanetContext context)
-        {
-            _context = context;
-        }
-
-        public ApplicationRole ApplicationRole { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            ApplicationRole = await _context.Roles.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (ApplicationRole == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
-    }
+	public class DetailsModel : PageModel
+	{
+		private readonly RoleManager<ApplicationRole> _roleManager;
+		public DetailsModel(RoleManager<ApplicationRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+		public ApplicationRole ApplicationRole { get; set; }
+		public async Task<IActionResult> OnGetAsync(string id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			ApplicationRole = await _roleManager.FindByIdAsync(id);
+			if (ApplicationRole == null)
+			{
+				return NotFound();
+			}
+			return Page();
+		}
+	}
 }
