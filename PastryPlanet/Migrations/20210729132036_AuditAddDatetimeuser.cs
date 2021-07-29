d4519f8a1fe45ef7d1d79e0c24e8c2835afa991b
x89302374 <s10204879@connect.np.edu.sg>
2021-07-29 22:37:37 +0800

    updated audit migrations

---
+++ PastryPlanet/Migrations/20210729132036_AuditAddDatetimeuser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PastryPlanet.Migrations
+{
+    public partial class AuditAddDatetimeuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTimeStamp",
+                table: "ProductAudit",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "ProductAudit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateTimeStamp",
+                table: "ProductAudit");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "ProductAudit");
+        }
+    }
+}
