73be2513eb296de3e6a92c455e86cd6ed3c33a2f
x89302374 <s10204879@connect.np.edu.sg>
2021-07-29 22:35:49 +0800

    updated for audit

--- PastryPlanet/Pages/Products/Delete.cshtml.cs
+++ PastryPlanet/Pages/Products/Delete.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -13,10 +14,13 @@ namespace PastryPlanet.Pages.Products
     public class DeleteModel : PageModel
     {
         private readonly PastryPlanet.Data.PastryPlanetContext _context;
+        private readonly SignInManager<ApplicationUser> _SignInManager;
 
-        public DeleteModel(PastryPlanet.Data.PastryPlanetContext context)
+        public DeleteModel(PastryPlanet.Data.PastryPlanetContext context,
+            SignInManager<ApplicationUser> signInManager)
         {
             _context = context;
+            _SignInManager = signInManager;
         }
 
         [BindProperty]
@@ -52,18 +56,34 @@ namespace PastryPlanet.Pages.Products
                 _context.Product.Remove(Product);
                 //await _context.SaveChangesAsync();
 
-                // Once a record is deleted, create an audit record
-                if (await _context.SaveChangesAsync() > 0)
+                if (_SignInManager.IsSignedIn(User))
                 {
-                    var auditrecord = new AuditRecord();
-                    auditrecord.AuditActionType = "Delete Product Record";
-                    auditrecord.DateTimeStamp = DateTime.Now;
-                    auditrecord.KeyProductID = Product.ID;
-                    var userID = User.Identity.Name.ToString();
-                    auditrecord.Username = userID;
-                    _context.AuditRecords.Add(auditrecord);
-                    await _context.SaveChangesAsync();
+                    var prod = new ProductAudit
+                    {
+                        Name = Product.Name,
+                        Description = Product.Description,
+                        Price = Product.Price,
+                        QuantityInStock = Product.QuantityInStock,
+                        Image = Product.Image,
+                        Productid = Product.ID,
+                        DateTimeStamp = DateTime.Now,
+                        Username = User.Identity.Name.ToString()
+                    };
+                    _context.ProductAudit.Add(prod);
+                    // Once a record is deleted, create an audit record
+                    if (await _context.SaveChangesAsync() > 0)
+                    {
+                        var auditrecord = new AuditRecord();
+                        auditrecord.AuditActionType = "Delete Product Record";
+                        auditrecord.DateTimeStamp = DateTime.Now;
+                        auditrecord.KeyProductID = prod.ID;
+                        var userID = User.Identity.Name.ToString();
+                        auditrecord.Username = userID;
+                        _context.AuditRecords.Add(auditrecord);
+                        await _context.SaveChangesAsync();
+                    }
                 }
+                
             }
             return RedirectToPage("./Index");
         }
