c329b8785221c136b2237d3e025b57006f79becd
xinmin <s10204879@connect.np.edu.sg>
2021-07-30 22:57:41 +0800

    Update ChangePassword.cshtml.cs

--- PastryPlanet/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ PastryPlanet/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -42,11 +43,13 @@ namespace PastryPlanet.Areas.Identity.Pages.Account.Manage
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
+            [RegularExpression("^[a-zA-Z0-9@#$&]*$", ErrorMessage = "Please enter valid string.")]
             [Display(Name = "New password")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm new password")]
+            [RegularExpression("^[a-zA-Z0-9@#$&]*$", ErrorMessage = "Please enter valid string.")]
             [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
@@ -70,6 +73,7 @@ namespace PastryPlanet.Areas.Identity.Pages.Account.Manage
 
         public async Task<IActionResult> OnPostAsync()
         {
+           
             if (!ModelState.IsValid)
             {
                 return Page();
