1c7d0261412efa14e9f444362ca9200f9f6eeacc
xinmin <s10204879@connect.np.edu.sg>
2021-07-31 11:05:37 +0800

    add product to cart

--- PastryPlanet/Pages/Products/Details.cshtml.cs
+++ PastryPlanet/Pages/Products/Details.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -13,13 +14,23 @@ namespace PastryPlanet.Pages.Products
     public class DetailsModel : PageModel
     {
         private readonly PastryPlanet.Data.PastryPlanetContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public DetailsModel(PastryPlanet.Data.PastryPlanetContext context)
+        public DetailsModel(PastryPlanet.Data.PastryPlanetContext context,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
         {
             _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public Product Product { get; set; }
+        public ProductInput Input { get; set; }
+        public CartItem CartItem { get; set; }
+        public cart cart { get; set; }
+        public ApplicationUser user { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -29,6 +40,12 @@ namespace PastryPlanet.Pages.Products
             }
 
             Product = await _context.Product.FirstOrDefaultAsync(m => m.ID == id);
+            ApplicationUser user = await _userManager.GetUserAsync(User);
+
+            if (_signInManager.IsSignedIn(User))
+            {
+                cart = await _context.Cart.FirstOrDefaultAsync(c => c.UserID == user.Id);
+            }
 
             if (Product == null)
             {
@@ -36,5 +53,71 @@ namespace PastryPlanet.Pages.Products
             }
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync(int id, ProductInput Input)
+        {
+            user = await _userManager.GetUserAsync(User);
+
+            
+            //get cart by user id 
+            cart = await _context.Cart.FirstOrDefaultAsync(c => c.UserID == user.Id);
+            Product = await _context.Product.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (Input.Quantity == 0)
+            {
+                return Page();
+            }
+                
+            CartItem CartItem = new CartItem
+            {
+                CartID = cart.ID,
+                ProductID = Product.ID,
+                Quantity = Input.Quantity
+            };       
+            if (await GetCartItemByProductIdForUserAsync(user.Id, id) != null)
+            {
+                CartItem existingCartItem = await GetCartItemByProductIdForUserAsync(user.Id, id);
+                existingCartItem.Quantity += Input.Quantity;
+                await UpdateCartItemsAsync(existingCartItem);
+            }
+            else
+            {
+                await CreateCartItemAsync(CartItem);
+            }
+            
+            return Redirect("/Cart");
+        }
+
+
+
+        public async Task<IEnumerable<CartItem>> GetCartItemsByUserIdAsync(string userId)
+        {
+            cart = await _context.Cart.FirstOrDefaultAsync(cart => cart.UserID == userId);
+            return _context.CartItem.Where(cartItem => cartItem.CartID == cart.ID).Include(x => x.Product);
+        }
+        public async Task<CartItem> GetCartItemByProductIdForUserAsync(string userId, int productId)
+        {
+            var cartItems = await GetCartItemsByUserIdAsync(userId);
+            return cartItems.FirstOrDefault(cart => cart.ProductID == productId);
+        }
+        public async Task UpdateCartItemsAsync(CartItem cartItem)
+        {
+            _context.CartItem.Update(cartItem);
+            await _context.SaveChangesAsync();
+        }
+        public async Task CreateCartItemAsync(CartItem cartItems)
+        {
+            await _context.CartItem.AddAsync(cartItems);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+    public class ProductInput
+    {
+        public int Quantity { get; set; } = 1;
     }
 }
