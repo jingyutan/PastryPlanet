98b37aced068647c6c1eb46bdd60bbf681320199
xinmin <s10204879@connect.np.edu.sg>
2021-07-31 16:26:58 +0800

    Update Register.cshtml.cs

--- PastryPlanet/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PastryPlanet/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -57,11 +57,13 @@ namespace PastryPlanet.Areas.Identity.Pages.Account
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
+            [RegularExpression("^[a-zA-Z0-9@#$&]*$", ErrorMessage = "Please enter valid string.")]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
+            [RegularExpression("^[a-zA-Z0-9@#$&]*$", ErrorMessage = "Please enter valid string.")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
